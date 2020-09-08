84c889b803dd824d729ce6d7cd2f127eb0065fed
thelittlecitizen16 <thelittlecitizen16@gmail.com>
2020-09-08 13:49:11 +0300

    add common

---
+++ Common/Enums/ClientOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Enums
+{
+    enum ClientOptions
+    {
+        GLOBAL_CHAT,
+        PRIVATE_CHAT,
+        GROUP_CHAT
+    }
+}
