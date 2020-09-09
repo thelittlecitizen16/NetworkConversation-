ca1d12dd9263f78b9cc22364cbeb76e6c95a160b
thelittlecitizen16 <thelittlecitizen16@gmail.com>
2020-09-09 10:35:48 +0300

    aff function in class creategroup chat in server

--- ServerNetworkConversation/Options/CreateGroupChat.cs
+++ ServerNetworkConversation/Options/CreateGroupChat.cs
@@ -38,28 +38,42 @@ namespace ServerNetworkConversation.Options
 
             try
             {
-                List<Guid> clientsToAdd = _data.ClientsConnectedInServer.Clients.Keys.ToList();
-                clientsToAdd.Remove(clientGuid);
+                SendAllConecctedClients(clientGuid);
 
-                Participants participants = new Participants(clientsToAdd);
-                _handleClient.SendToClient(_client, participants);
+                GroupChat groupChat = WaitToGetGroupFromClient();
 
-                GroupChat groupChat = (GroupChat)_handleClient.GetFromClient(_client);
-
-                while (groupChat == null)
-                {
-                    groupChat = (GroupChat)_handleClient.GetFromClient(_client);
-                }
-                var guidClient = _data.ClientsConnectedInServer.GetGuid(_client);
-
-                groupChat.Managers.Add(guidClient);
-                groupChat.Participants.Add(guidClient);
-                _data.AllGroupsChat.groupsChat.Add(groupChat);
+                AddGroup(groupChat, clientGuid);
             }
             catch (Exception)
             {
                 _removeClient.RemoveClientWhenOut(_client, clientGuid);
             }
+        }
+
+        private void SendAllConecctedClients(Guid clientGuid)
+        {
+            List<Guid> clientsToAdd = _data.ClientsConnectedInServer.Clients.Keys.ToList();
+            clientsToAdd.Remove(clientGuid);
+
+            Participants participants = new Participants(clientsToAdd);
+            _handleClient.SendToClient(_client, participants);
+        }
+        private GroupChat WaitToGetGroupFromClient()
+        {
+            GroupChat groupChat = (GroupChat)_handleClient.GetFromClient(_client);
+
+            while (groupChat == null)
+            {
+                groupChat = (GroupChat)_handleClient.GetFromClient(_client);
+            }
+
+            return groupChat;
+        }
+        private void AddGroup(GroupChat groupChat, Guid clientGuid)
+        {
+            groupChat.Managers.Add(clientGuid);
+            groupChat.Participants.Add(clientGuid);
+            _data.AllGroupsChat.groupsChat.Add(groupChat);
         }
     }
 }
